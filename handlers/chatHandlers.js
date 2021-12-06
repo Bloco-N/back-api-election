ca7440fc298c8b2498587cd5c2e17a2d8e2436d7
ilwel <ilwelisaac@hotmail.com>
2021-12-06 16:59:48 -0300

    base app

---
+++ handlers/chatHandlers.js
@@ -0,0 +1,22 @@
+const db = require('../db/db');
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = (io, socket) => {
+
+  const sendMessage = async (data) => {
+
+    const id = uuidv4();
+    const { content, user_id, type } = data;
+
+    console.log(`[SOCKET] new message:`);
+    console.log(data);
+
+    const result = await db('messages').insert({ id, user_id, content, type });
+    if (result.rowCount > 0) {
+      io.emit('chat:receive-message', { id, ...data });
+    }
+  }
+
+  socket.on('chat:send-message', sendMessage);
+
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+require('dotenv').config(); // dontenv config
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const io = require('socket.io')(server);
+
+const registerChatHandlers = require('./handlers/chatHandlers');
+const registerUserHandlers = require('./handlers/userHandlers');
+
+const onConnection = (socket) => {
+  try {
+
+    registerChatHandlers(io, socket);
+    registerUserHandlers(io, socket);
+
+  } catch (error) {
+    console.log(error.message);
+  }
+
+}
+
+io.on('connection', onConnection);
+
+server.listen(process.env.PORT, () => {
+  console.log(`running api`)
+})
