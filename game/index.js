2dee6bbe1b0902e916bbba2d3a755111ae6b83a0
Aldebaran <elton.alde@gmail.com>
2021-12-07 13:02:13 -0300

    game instance

---
+++ game/index.js
@@ -0,0 +1,19 @@
+
+
+class Game {
+
+    constructor()
+    {
+        this.players = [];
+    }
+
+    init()
+    {
+        console.log(this.players)
+    }
+
+}
+
+const game = new Game();
+
+module.exports = game;
