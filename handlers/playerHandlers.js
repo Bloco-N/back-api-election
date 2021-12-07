4812950103113b993565ecebca380f0200faf528
Aldebaran <elton.alde@gmail.com>
2021-12-07 13:13:14 -0300

    player handlers

---
+++ handlers/playerHandlers.js
@@ -0,0 +1,22 @@
+const game = require('../game')
+
+module.exports = (io, socket) => {
+
+    const add = (data) => {
+        
+        game.players.push(data);
+
+    }
+
+    const remove = (data) =>{
+
+        const index = game.players.indexOf(data.user_id);
+
+        game.players.splice(index, 1);
+
+    }
+
+    socket.on('player:add', add);
+    socket.on('player:remove', remove);
+
+}
